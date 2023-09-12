68955eab77654e19cf41f245a9df61aec45d0f24
Okazaki <net.kohei.okazaki@gmail.com>
2023-09-12 10:54:04 +0900

    change

--- provision/tc/ip.sh
+++ provision/tc/ip.sh
@@ -1,17 +1,31 @@
 #!/bin/bash
 
-# Define the desired bandwidth limit in Mbps
-initial_bandwidth_mbps=100
-delay_ms=5
-network_interface="enp0s8"
+# Get the value of CONFIG_HZ from the kernel configuration
+config_hz=$(cat /boot/config-`uname -r` | grep CONFIG_HZ= | cut -d "=" -f2)
 
-# Check if a qdisc is already attached to the specified network interface
-if ! tc qdisc show dev "$network_interface" | grep -q "netem"; then
-  # No qdisc found, so add a new one with the specified delay and bandwidth limit
-  tc qdisc add dev "$network_interface" root handle 1:0 netem delay "${delay_ms}ms"
-  tc qdisc add dev "$network_interface" parent 1:1 handle 10: tbf rate "${initial_bandwidth_mbps}mbit" burst 32kbit
-else
-  # Qdisc already exists, so update the delay and bandwidth limit
-  tc qdisc change dev "$network_interface" root netem delay "${delay_ms}ms"
-  tc qdisc change dev "$network_interface" parent 1:1 handle 10: tbf rate "${initial_bandwidth_mbps}mbit" burst 32kbit
-fi
+# Calculate burst and limit based on rate and CONFIG_HZ
+bandwidth_mbps=100  # Adjust this value as needed
+burst=$((bandwidth_mbps * 1000 / config_hz))  # Convert rate to kbit and divide by CONFIG_HZ
+limit=$((burst * 10))
+
+# Define the desired bandwidth limit in Mbps
+delay_ms=5
+network_interface1="enp0s8"
+
+# Function to configure a network interface
+configure_network_interface() {
+  local interface="$1"
+  # Check if a qdisc is already attached to the network interface
+  if ! tc qdisc show dev "$interface" | grep -q "netem"; then
+    # No qdisc found, so add a new one with the specified delay and bandwidth limit
+    tc qdisc add dev "$interface" root handle 1:0 netem delay "${delay_ms}ms"
+    tc qdisc add dev "$interface" parent 1:1 handle 10: tbf rate "${bandwidth_mbps}mbit" burst "${burst}kbit" limit "${limit}kbit"
+  else
+    # Qdisc already exists, so update the delay and bandwidth limit
+    tc qdisc change dev "$interface" root netem delay "${delay_ms}ms"
+    tc qdisc change dev "$interface" parent 1:1 handle 10: tbf rate "${bandwidth_mbps}mbit" burst "${burst}kbit" limit "${limit}kbit"
+  fi
+}
+
+# Configure the first network interface
+configure_network_interface "$network_interface1"
--- provision/tc/proxy.sh
+++ provision/tc/proxy.sh
@@ -1,29 +1,35 @@
 #!/bin/bash
 
+# Get the value of CONFIG_HZ from the kernel configuration
+config_hz=$(cat /boot/config-`uname -r` | grep CONFIG_HZ= | cut -d "=" -f2)
+
+# Calculate burst and limit based on rate and CONFIG_HZ
+bandwidth_mbps=100  # Adjust this value as needed
+burst=$((bandwidth_mbps * 1000 / config_hz))  # Convert rate to kbit and divide by CONFIG_HZ
+limit=$((burst * 10))
+
 # Define the desired bandwidth limit in Mbps
-initial_bandwidth_mbps=100
 delay_ms=5
 network_interface1="enp0s8"
 network_interface2="enp0s9"  # Add the second network interface here
 
-# Check if a qdisc is already attached to the first network interface
-if ! tc qdisc show dev "$network_interface1" | grep -q "netem"; then
-  # No qdisc found, so add a new one with the specified delay and bandwidth limit
-  tc qdisc add dev "$network_interface1" root handle 1:0 netem delay "${delay_ms}ms"
-  tc qdisc add dev "$network_interface1" parent 1:1 handle 10: tbf rate "${initial_bandwidth_mbps}mbit" burst 32kbit
-else
-  # Qdisc already exists, so update the delay and bandwidth limit
-  tc qdisc change dev "$network_interface1" root netem delay "${delay_ms}ms"
-  tc qdisc change dev "$network_interface1" parent 1:1 handle 10: tbf rate "${initial_bandwidth_mbps}mbit" burst 32kbit
-fi
+# Function to configure a network interface
+configure_network_interface() {
+  local interface="$1"
+  # Check if a qdisc is already attached to the network interface
+  if ! tc qdisc show dev "$interface" | grep -q "netem"; then
+    # No qdisc found, so add a new one with the specified delay and bandwidth limit
+    tc qdisc add dev "$interface" root handle 1:0 netem delay "${delay_ms}ms"
+    tc qdisc add dev "$interface" parent 1:1 handle 10: tbf rate "${bandwidth_mbps}mbit" burst "${burst}kbit" limit "${limit}kbit"
+  else
+    # Qdisc already exists, so update the delay and bandwidth limit
+    tc qdisc change dev "$interface" root netem delay "${delay_ms}ms"
+    tc qdisc change dev "$interface" parent 1:1 handle 10: tbf rate "${bandwidth_mbps}mbit" burst "${burst}kbit" limit "${limit}kbit"
+  fi
+}
 
-# Check if a qdisc is already attached to the second network interface
-if ! tc qdisc show dev "$network_interface2" | grep -q "netem"; then
-  # No qdisc found, so add a new one with the specified delay and bandwidth limit
-  tc qdisc add dev "$network_interface2" root handle 1:0 netem delay "${delay_ms}ms"
-  tc qdisc add dev "$network_interface2" parent 1:1 handle 10: tbf rate "${initial_bandwidth_mbps}mbit" burst 32kbit
-else
-  # Qdisc already exists, so update the delay and bandwidth limit
-  tc qdisc change dev "$network_interface2" root netem delay "${delay_ms}ms"
-  tc qdisc change dev "$network_interface2" parent 1:1 handle 10: tbf rate "${initial_bandwidth_mbps}mbit" burst 32kbit
-fi
+# Configure the first network interface
+configure_network_interface "$network_interface1"
+
+# Configure the second network interface
+configure_network_interface "$network_interface2"
